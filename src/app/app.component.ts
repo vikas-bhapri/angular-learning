f461bc7d35782f87313a9182662be36e20c83904
whitepanther2000 <vikasbhapri@gmail.com>
2023-08-18 17:26:33 +0530

    Template Driven Forms Completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { PostComponent } from './post/post.component';
 import { AppendPipe } from './Pipes/append.pipe';
+import { FormControl, FormGroup, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -8,20 +9,13 @@ import { AppendPipe } from './Pipes/append.pipe';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = "Angular Course";
-  number = 1234567.89123;
-  price = 99.9;
-  today: Date = new Date();
-  postObject = {
-    id: 1,
-    postTitle: "post 1"
+  onSubmit(data:NgForm){
+    console.log(data.value);
+    
   }
-  postArray = ['post 1', 'post 2', 'post 3', 'post 4']
-  userDetails = {
-    name: 'user1',
-    city: 'bengaluru',
-    country:'ind'
 
+  getValue(full:any){
+    console.log(full);
+    
   }
-  dummyText = "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quo beatae mollitia aperiam placeat accusantium explicabo nesciunt voluptatibus eveniet quod debitis! Cupiditate laborum eligendi voluptatibus nostrum dignissimos dolores tempore recusandae ab? Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quo beatae mollitia aperiam placeat accusantium explicabo nesciunt voluptatibus eveniet quod debitis! Cupiditate laborum eligendi voluptatibus nostrum dignissimos dolores tempore recusandae ab?" 
 }
